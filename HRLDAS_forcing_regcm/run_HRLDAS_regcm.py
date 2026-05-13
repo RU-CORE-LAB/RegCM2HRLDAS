a9916a4e1dd1b9e6fc5779190e4466a0c25add64
RU-CORE-LAB <167742783+RU-CORE-LAB@users.noreply.github.com>
2026-05-13 10:29:00 +0700

    Update run_HRLDAS_regcm.py
    fix: remove calendar argument from run_hrldas_regcm.py and auto-detect calendar in create_forcing_prl.py

--- HRLDAS_forcing_regcm/run_HRLDAS_regcm.py
+++ HRLDAS_forcing_regcm/run_HRLDAS_regcm.py
@@ -45,17 +45,17 @@ geo_em_file = '/home/admin/WRF/RUN/LSP-DS/Data_114/run_regcm/geo_em.d01.nc'
 #exe_directory = '../hrldas/hrldas/run/'
 levelist = '136'
 ZLVL = 30
-calendar="gregorian"
+num_cores = 20 
 
 
 create_lai_vegfra(geo_em_file, dir_hrldas)
 print("LAI : OK")
 create_setup_file(f'{str(start_year)}-{loop_start_date}',dir_raw, dir_hrldas,geo_em_file)
 print("create_setup_file : OK" )
-num_cores = 20
+
 
 for year in range(start_year, end_year+1):
     create_LDASIN_files_parallel(f'{str(year)}-{loop_start_date}', f'{str(year)}-{loop_end_date}', \
                         dir_raw, dir_hrldas, \
-                        geo_em_file, gcm_name,scenario ,levelist, ZLVL, calendar, num_cores)
+                        geo_em_file, gcm_name,scenario ,levelist, ZLVL, num_cores)
 
